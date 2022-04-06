b7f48997bea99cefb4f398468f2d732279081122
Mukkund <mukkundsunjii.babugheethaa@ortec.com>
2022-04-06 14:23:04 +0200

    changed host name for notekeeper

--- apps/keeper/src/components/NoteKeeper.jsx
+++ apps/keeper/src/components/NoteKeeper.jsx
@@ -1,4 +1,4 @@
-const api_host = process.env.REACT_APP_NOTEKEEPER_HOSTIP || "localhost";
+const api_host = process.env.REACT_APP_NOTEKEEPER_HOSTIP || "notekeeper";
 const api_port = process.env.REACT_APP_NOTEKEEPER_DOCKER_PORT || 4000
 const noteKeeperUri = `http://${api_host}:${api_port}/api/v1/`;
 export default noteKeeperUri;
